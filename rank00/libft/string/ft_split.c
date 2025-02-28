8a46d8f31a20111fd8009e511886e7acb0caf416
JuHyeonLee <ljh3900@gmail.com>
2025-02-28 05:22:17 +0200

    add

--- rank00/libft/string/ft_split.c
+++ rank00/libft/string/ft_split.c
@@ -6,54 +6,82 @@
 /*   By: JuHyeon <juhyeonl@student.hive.fi>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/03 12:38:12 by JuHyeon           #+#    #+#             */
-/*   Updated: 2025/02/27 17:09:18 by JuHyeon          ###   ########.fr       */
+/*   Updated: 2025/02/28 01:05:01 by JuHyeon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-int	cnt_deli(char const *s, char c)
+static void	free_arr(char **arr, int i)
 {
-	size_t	cnt;
+	while (i >= 0)
+	{
+		free(arr[i]);
+		i--;
+	}
+	free(arr);
+}
 
-	if (!*s)
+/* 문자열 내 단어 개수 세기 */
+static int	cnt_deli(char const *s, char c)
+{
+	int	cnt;
+
+	if (!s || !*s)
 		return (0);
 	cnt = 0;
 	while (*s)
 	{
-		while (*s == c)
+		while (*s && *s == c)
 			s++;
 		if (*s)
 			cnt++;
-		while (*s != c && *s)
+		while (*s && *s != c)
 			s++;
 	}
 	return (cnt);
 }
 
+/* 단어를 분리하여 저장 */
+static char	*get_word(const char **s, char c)
+{
+	size_t		len;
+	char		*word;
+	const char	*end;
+
+	while (**s && **s == c)
+		(*s)++;
+	end = ft_strchr(*s, c);
+	if (end)
+		len = end - *s;
+	else
+		len = ft_strlen(*s);
+	word = ft_substr(*s, 0, len);
+	*s += len;
+	return (word);
+}
+
+/* 문자열을 구분자로 나누어 배열로 반환 */
 char	**ft_split(char const *s, char c)
 {
 	int		i;
-	size_t	w_len;
 	char	**arr;
 
+	if (!s)
+		return (NULL);
 	arr = (char **)malloc((cnt_deli(s, c) + 1) * sizeof(char *));
-	if (!s || !arr)
-		return (0);
+	if (!arr)
+		return (NULL);
 	i = 0;
 	while (*s)
 	{
-		while (*s == c && *s)
-			s++;
-		if (*s)
+		arr[i] = get_word(&s, c);
+		if (!arr[i])
 		{
-			if (!ft_strchr(s, c))
-				w_len = ft_strlen(s);
-			else
-				w_len = ft_strchr(s, c) - s;
-			arr[i++] = ft_substr(s, 0, w_len);
-			s += w_len;
+			free_arr(arr, i - 1);
+			return (NULL);
 		}
+		i++;
 	}
 	arr[i] = NULL;
 	return (arr);
